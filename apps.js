1694e4cb7cd4737a09bce70e4cce68b2913eb928
Juan David Arango <davidarango.ro@gmail.com>
2022-07-14 11:59:36 -0500

    feat: added app.js

---
+++ apps.js
@@ -0,0 +1,51 @@
+//Aquí se editan o agregan las aplicaciones
+const agregarAplicaciones = [
+  {
+    title: "Simuladores",
+    url: "https://www.google.com",
+    isActive: true, // Se manda "true" si se quiere mostrar, sino se manda "false"
+    image: "./images/logo-simuladores.png", //Se envía la ruta de la imagen
+  },
+  {
+    title: "Valle Mágico",
+    url: "https://www.google.com",
+    isActive: false,
+    image: "./images/logo-vallemagico.png",
+  },
+  {
+    title: "Wikipedia",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/wikipedia.png",
+  },
+  {
+    title: "Moodle",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/moodle.png",
+  },
+  {
+    title: "Colcha de tesoros",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/logo-colchadetesoros.png",
+  },
+  {
+    title: "Ejemplo 1",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/logo.png",
+  },
+  {
+    title: "Ejemplo 2",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/logo.png",
+  },
+  {
+    title: "Ejemplo 3",
+    url: "https://www.google.com",
+    isActive: true,
+    image: "./images/logo.png",
+  },
+];
