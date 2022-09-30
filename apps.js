a4d1ff2005ab71713a22ab16a28cb2ff9e0653f3
Lina Asprilla <linaasprilla@hotmail.com>
2022-09-30 16:37:41 -0500

    change styles

--- apps.js
+++ apps.js
@@ -4,7 +4,10 @@ const agregarAplicaciones = [
     title: "Simuladores",
     url: "https://www.google.com",
     isActive: true, // Se manda "true" si se quiere mostrar, sino se manda "false"
-    image: "/images/tumlab_simuladores.png", //Se envía la ruta de la imagen
+    image: "/images/tumlab_simuladores.png", //Se envía la ruta de la imagen,
+    description:
+      "Simulaciones interactivas en Física, Química, Ciencias basada en el proyecto de simulaciones interactivas de PhET de la Universidad de Colorado en Boulder. Estos recursos nacen de la investigación educativa extensiva e involucran a los estudiantes mediante un ambiente intuitivo y similar a un juego, en donde aprenden explorando y descubren.",
+    image_background: "/images/simuladores_bg.png",
   },
   {
     title: "Valle Mágico",
@@ -17,6 +20,10 @@ const agregarAplicaciones = [
     url: "https://www.google.com",
     isActive: true,
     image: "/images/wikipedia_tumlab.png",
+    image_title: "/images/Wikipedia-Logo.png",
+    description:
+      "Wikipedia es una página web para realizar consultas online de contenido abierto, una web que almacena, recopila y transmite información de manera totalmente estructurada. Cualquier persona registrada puede participar en esta particular enciclopedia y publicar información o artículos que puedan ayudar a otras personas a encontrar los datos o información que necesitan",
+    image_background: "/images/wikipedia_bg.png",
   },
   {
     title: "Moodle",
@@ -35,6 +42,10 @@ const agregarAplicaciones = [
     url: "https://www.google.com",
     isActive: true,
     image: "/images/tumlab_lms.png",
+    image_title: "/images/title_tumlab.png",
+    description:
+      "TUMLAB LMS es una plataforma de aprendizaje diseñada para proporcionarle a educadores, administradores y estudiantes un sistema integrado único, robusto y seguro para crear ambientes de aprendizaje personalizados. Una interfaz simple, recursos bien documentados, junto con mejoras contiuas, hacen a Moodle fácil de aprender y usar.",
+    image_background: "/images/tumlab_bg.png",
   },
   {
     title: "Ejemplo 2",
@@ -63,37 +74,35 @@ const showApps = () => {
   });
 };
 
-//Acciones
+//Muestra el contenido al presionar la aplicación
 const onClickApp = (data) => {
   const key = data.getAttribute("key");
   document.getElementById("logo").style.display = "none";
+  document.getElementById("h4_cards").style.display = "none";
   document.body.style.background =
     "linear-gradient(0.84deg, #393939 0.89%, rgba(249, 243, 243, 0) 47.35%)";
-  console.log("click", key);
-  switch (key) {
-    case "Wikipedia":
+
+  agregarAplicaciones.map((app) => {
+    if (app.isActive && app.title === key) {
       document.getElementById("content").innerHTML = `
-      <div class="container_content animate__animated animate__fadeIn animate__slow">
-      <div class="container_left"> 
-       <img src="/images/Wikipedia-Logo.png" alt="Wikipedia" />
-      <p>Wikipedia es una página web para realizar consultas online de contenido abierto, una web que almacena, recopila y transmite información de manera totalmente estructurada. Cualquier persona registrada puede participar en esta particular enciclopedia y publicar información o artículos que puedan ayudar a otras personas a encontrar los datos o información que necesitan</h1>
-      </div>
-      <div class="container_right">
-      <p>Hola</p>
-      </div>
-      </div>
-      `;
-      break;
-    case "Simuladores":
-      document.getElementById("content").innerHTML = `
-      <h1>Simuladores</h1>
-      `;
-      break;
-    case "Tumlab LMS":
-      document.getElementById("content").innerHTML = `
-      <h1>Tumlab LMS</h1>
-      `;
-    default:
-      break;
-  }
+        <div class="container_content animate__animated animate__fadeIn animate__slower">
+        <div class="container_left">
+        ${
+          app.image_title
+            ? `<img src=${app.image_title} alt="Wikipedia" />`
+            : `<h1> ${app.title} </h1>`
+        }
+        <p class="p_container_left">${app.description}</p>
+        <a class="btn_go" href="${app.url}" target="_blank"> Iniciar <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.75 3.25L13.25 7.75L8.75 12.25M2.75 7.75H13.25" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        </a>
+        </div>
+        <div class="container_right">
+        <img src=${app.image_background} alt="${app.title} " />
+        </div>
+        </div>
+        `;
+    }
+  });
 };
