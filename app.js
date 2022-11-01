95c43458a05190a2ab4edda3c8261892c324f8c3
root <jr.granada@talentum.edu.co>
2022-11-01 11:41:22 -0500

    added domain to some apps

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const addApps = [
     cover: "./img/wiki/wiki_cover.png",
     description:
       "Wikipedia es una página web para realizar consultas online de contenido abierto, una web que almacena, recopila y transmite información de manera totalmente estructurada.",
-    url: "https://www.google.com",
+    url: "http://tumlab.local",
   },
   {
     key: 2,
@@ -18,7 +18,7 @@ const addApps = [
     cover: "./img/tumlab/tumlab_cover.png",
     description:
       "TUMLAB LMS es una plataforma de aprendizaje diseñada para proporcionarle a educadores, administradores y estudiantes un sistema integrado único, robusto y seguro para crear ambientes de aprendizaje personalizados. Una interfaz simple, recursos bien documentados, junto con mejoras continuas, hacen a Moodle fácil de aprender y usar.",
-    url: "https://www.google.com",
+    url: "http://tumlab.local:5000",
   },
   {
     key: 3,
@@ -28,7 +28,7 @@ const addApps = [
     cover: "./img/simuladores/simuladores_cover.png",
     description:
       "Simulaciones interactivas en Física, Química, Ciencias basada en el proyecto de simulaciones interactivas de PhET de la Universidad de Colorado en Boulder. Estos recursos nacen de la investigación educativa extensiva e involucran a los estudiantes mediante un ambiente intuitivo y similar a un juego, en donde aprenden explorando y descubren.",
-    url: "https://www.google.com",
+    url: "http://tumlab.local",
   },
   {
     key: 4,
@@ -37,25 +37,25 @@ const addApps = [
     title: "",
     cover: "",
     description: "",
-    url: "https://www.google.com",
+    url: "http://tumlab.local",
   },
   {
     key: 5,
     thumbnail: "./img/colcha/colcha_thumbnail.png",
-    is_show: true,
+    is_show: false,
     title: "",
     cover: "./img/colcha/colcha_cover.png",
     description: "City Know pro 2 es la segunda versión del juego educativo City Know pro, donde los estudiantes desde transición hasta grado noveno aprenden jugando minijuegos, Inglés, Matemáticas, Lenguaje, competencia ciudadana son las distintas disciplinas que el niño aprenderá jugando, el juego esta diseñado con una accesibilidad fluida, así cualquier niño sin importar su discapacidad puede aprender jugando.",
-    url: "https://colchadetesoros.com/",
+    url: "http://tumlab.local",
   },
   {
     key: 6,
     thumbnail: "./img/valle/valle_thumbnail.png",
-    is_show: true,
+    is_show: false,
     title: "",
     cover: "./img/valle/valle_cover.png",
     description: "Valle Mágico es una propuesta educativa inclusiva para el desarrollo de las capacidades y talentos de los estudiantes de las 149 instituciones educativas oficiales, de los 34 municipios no certificados del departamento del Valle del Cauca. Este proyecto garantiza una atención educativa con calidad, en el marco de la educación inclusiva correspondiente al decreto 366 en el artículo 10 para las prácticas educativas inclusivas.",
-    url: "https://vallemagico.co/",
+    url: "http://tumlab.local",
   },
   {
     key: 7,
@@ -65,7 +65,7 @@ const addApps = [
     cover: "./img/superset/superset_cover.png",
     description:
       "Superset es rápido, liviano, intuitivo y está cargado de opciones que facilitan a los usuarios de todos los conjuntos de habilidades explorar y visualizar sus datos, desde simples gráficos de líneas hasta gráficos geoespaciales altamente detallados.",
-    url: "https://superset.apache.org/",
+    url: "http://tumlab.local:8088",
   },
   {
     key: 8,
@@ -75,7 +75,7 @@ const addApps = [
     cover: "./img/scratch/scratch_cover.png",
     description:
       "Scratch es la comunidad de programación para niños y niñas más grande del mundo, y un lenguaje de programación con una interfaz sencilla que permite a los jóvenes crear historias digitales, juegos y animaciones. Scratch está diseñado, desarrollado y moderado por la Fundación Scratch, una organización sin ánimo de lucro. Scratch promueve el pensamiento computacional y las habilidades en resolución de problemas; enseñanza y aprendizaje creativos, auto expresión y colaboración; e igualdad en informática.",
-    url: "https://scratch.mit.edu/",
+    url: "http://tumlab.local",
   },
 ];
 
